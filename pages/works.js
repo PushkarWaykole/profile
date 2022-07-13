5cb6b6864d00e731607b55e62e6fd53a9e2e0735
Pushkar Waykole <pushkarwaykole123@gmail.com>
2022-07-13 13:09:08 +0530

    deploy

--- pages/works.js
+++ pages/works.js
@@ -6,13 +6,7 @@ import { WorkGridItem } from '../components/grid-item'
 import newflix from '../public/images/works/newflix.png'
 import image_cropper from '../public/images/works/image_cropper.png'
 import flash from '../public/images/works/flash1.png'
-import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
-import thumbMargelo from '../public/images/works/margelo_eyecatch.png'
-import thumbModeTokyo from '../public/images/works/modetokyo_eyecatch.png'
-import thumbStyly from '../public/images/works/styly_eyecatch.png'
-import thumbPichu2 from '../public/images/works/pichu2_eyecatch.png'
-import thumbFreeDBTagger from '../public/images/works/freedbtagger_eyecatch.png'
-import thumbAmembo from '../public/images/works/amembo_eyecatch.png'
+
 
 const Works = () => (
   <Layout title="Works">
@@ -23,7 +17,7 @@ const Works = () => (
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
         <Section>
-          <WorkGridItem id="inkdrop" title="NewFlix" thumbnail={newflix}>
+          <WorkGridItem id="newflix" title="NewFlix" thumbnail={newflix}>
             A Movie recommender app with categories.
           </WorkGridItem>
         </Section>
